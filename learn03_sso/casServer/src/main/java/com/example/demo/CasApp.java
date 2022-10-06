23cf325abaeca0c3bef4bf88a0b62d91c38ed8dd
zxz <aabbcc5050@qq.com>
2022-10-06 22:16:09 +0800

    Changes

--- learn03_sso/casServer/src/main/java/com/example/demo/CasApp.java
+++ learn03_sso/casServer/src/main/java/com/example/demo/CasApp.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 参考:https://mp.weixin.qq.com/s/TBVtYPrbLMJyaUulLSVx9g
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CasApp {
 
